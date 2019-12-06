aa2e402eaa667ee8ffca30d06c86f38cf32c5199
zainkhanx93 <zainkhanx93@gmail.com>
2019-12-06 01:48:39 -0800

    Update ViewController.swift

--- Parked/Parked/Parked/ViewController.swift
+++ Parked/Parked/Parked/ViewController.swift
@@ -1,71 +1,77 @@
 //
 //  ViewController.swift
-//  Parked
+//  1
 //
-//  Created by Zain Khan on 12/5/19.
+//  Created by Zain Khan on 12/6/19.
 //  Copyright © 2019 Zain Khan. All rights reserved.
 //
 
 import UIKit
 import MapKit
-import CoreLocation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Map: MKMapView!
+    
+    override func viewDidLoad()
+      {
+          super.viewDidLoad()
+        //  checkLocationServices()
+      }
+      
 
-    @IBOutlet weak var ParkedMap: MKMapView!
+
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
         
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        checkLocationServices()
-    }
-    
+  
     func setupLocationManager()
-    {
-        locationManager.delegate = (self as! CLLocationManagerDelegate)
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    }
-    func centerViewOnUserLocation()
-    {
-        if let location = locationManager.location?.coordinate
         {
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-            ParkedMap.setRegion(region, animated: true)
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
+
+        func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+                    setupLocationManager()
+                    checkLocationAuthorization()
+                } else {
+                    // Show alert letting the user know they have to turn this on.
+                }
+            }
+            
+            
+            func checkLocationAuthorization() {
+                switch CLLocationManager.authorizationStatus(){
+                case .authorizedWhenInUse:
+                    Map.showsUserLocation = true
+                  //  centerViewOnUserLocation()
+                    locationManager.startUpdatingLocation()
+                    break
+                case .denied:
+                    // Show alert instructing them how to turn on permissions
+                    break
+                case .notDetermined:
+                    locationManager.requestWhenInUseAuthorization()
+                case .restricted:
+                    // Show an alert letting them know what's up
+                    break
+                case .authorizedAlways:
+                    break
+                }
+            }
+        
+        }
+
+
+    extension ViewController: CLLocationManagerDelegate {
+        
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
             
         }
         
-    }
-    func checkLocationServices() {
-    if CLLocationManager.locationServicesEnabled() {
-                setupLocationManager()
-                checkLocationAuthorization()
-            } else {
-                // Show alert letting the user know they have to turn this on.
-            }
-        }
         
-        
-        func checkLocationAuthorization() {
-            switch CLLocationManager.authorizationStatus()
-            {
-            case .authorizedWhenInUse:
-                ParkedMap.showsUserLocation = true
-                centerViewOnUserLocation()
-                locationManager.startUpdatingLocation()
-                break
-            case .denied:
-                // Show alert instructing them how to turn on permissions
-                break
-            case .notDetermined:
-                locationManager.requestWhenInUseAuthorization()
-            case .restricted:
-                // Show an alert letting them know what's up
-                break
-            case .authorizedAlways:
-                break
-            }
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            //checkLocationAuthorization()
         }
-    
     }
+
